a18e7d8b84bd583cbe2b9f39eb4cdf23fbd1ca14
Szymon Papuga <papuga.szymon@gmail.com>
2024-04-21 11:53:16 +0200

    infinite scrolling

--- src/lib/Feed.svelte
+++ src/lib/Feed.svelte
@@ -1,11 +1,29 @@
 <script>
 	import Tweet from './Tweet.svelte';
+	import InfiniteScroll from 'svelte-infinite-scroll';
 
 	export let tweets;
+	let newTweetsLen = tweets.length;
+	$: cursor = tweets[tweets.length - 1].id;
+
+	async function fetchTweets() {
+		const response = await fetch(`http://localhost:5173/tweets?cursor=${cursor}`);
+		const newTweets = await response.json();
+		tweets = [...tweets, ...newTweets];
+		newTweetsLen = newTweets.length;
+	}
 </script>
 
 <div class="w-2/5">
 	{#each tweets as tweet (tweet.id)}
 		<Tweet {tweet} />
 	{/each}
+	<InfiniteScroll
+		hasMore={newTweetsLen}
+		threshold={100}
+		window={true}
+		on:loadMore={() => {
+			fetchTweets();
+		}}
+	/>
 </div>
