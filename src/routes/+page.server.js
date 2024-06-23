d28bb60e13af96ad17f69a7069caae1d224fe0d0
Szymon Papuga <papuga.szymon@gmail.com>
2024-06-23 16:10:32 +0200

    server action (non-API approach)

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,15 +1,22 @@
-import prisma from '$lib/prisma'
+import prisma from '$lib/prisma';
 
 export async function load() {
-    return {
-        tweets: await prisma.tweet.findMany({
-            include: {
-                user: true
-            },
-            take: 20,
-            orderBy: {
-                creationTime: 'desc',
-            }
-        })
-    }
+	return {
+		tweets: await prisma.tweet.findMany({
+			include: {
+				user: true
+			},
+			take: 20,
+			orderBy: {
+				creationTime: 'desc'
+			}
+		})
+	};
 }
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		console.log(data.get('text'));
+	}
+};
