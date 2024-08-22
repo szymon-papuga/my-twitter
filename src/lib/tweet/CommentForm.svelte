f8dd8ee7d291a755af0fc6569d59a7b8510b3e2e
Szymon Papuga <papuga.szymon@gmail.com>
2024-08-22 09:22:31 +0200

    add person to comment form

--- src/lib/tweet/CommentForm.svelte
+++ src/lib/tweet/CommentForm.svelte
@@ -2,6 +2,7 @@
 	import { enhance } from '$app/forms';
 	import { createEventDispatcher } from 'svelte';
 	import autosize from 'svelte-autosize';
+	import Person from 'virtual:icons/material-symbols/person';
 
 	export let tweetId;
 
@@ -13,26 +14,27 @@
 	action="/tweets"
 	use:enhance
 	on:submit={() => dispatch('close')}
-	class="p-4 gap-x-3"
+	class="p-4"
 >
+		<Person />
 	<textarea
 		name="text"
 		placeholder="Add another tweet"
-		class="row-start-1 row-end-4 outline-none text-xl resize-none"
+		class="block outline-none text-xl resize-none w-full col-start-2 h-14"
 		use:autosize
 	/>
 	<input type="hidden" name="parentId" value={tweetId} />
 	<input
 		type="submit"
 		value="Submit"
-		class="text-white bg-sky-400 font-bold rounded-full rows-start-2 row-end-3"
+		class="text-white bg-sky-400 font-bold rounded-full float-right w-28 h-8 col-start-3"
 	/>
 </form>
 
 <style>
 	form {
 		display: grid;
-		grid-template-columns: 75% auto;
-		grid-template-rows: auto 70% auto;
+		grid-template-columns: max-content auto min-content;
+		gap: 8px;
 	}
 </style>
