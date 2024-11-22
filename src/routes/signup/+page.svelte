b06c55de18beaeaa096d0433ae83473b950ad7b4
Szymon Papuga <papuga.szymon@gmail.com>
2024-11-22 22:07:44 +0100

    confirm password on sign up page

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,5 +1,9 @@
 <script>
 	import { enhance } from '$app/forms';
+
+	let password = $state('');
+	let passwordConfirmed = $state('');
+	let passwordsNotEqual = $derived(password !== passwordConfirmed);
 </script>
 
 <form method="POST" use:enhance>
@@ -8,6 +12,19 @@
 	<label for="email">E-mail:</label><br />
 	<input type="email" id="email" name="email" required /><br />
 	<label for="password">Password:</label><br />
-	<input type="password" id="password" name="password" required /><br /><br />
-	<button type="submit">Sign Up</button>
+	<input bind:value={password} type="password" id="password" name="password" required /><br />
+	<label for="confirm_password">Confirm Password:</label><br />
+	<input
+		bind:value={passwordConfirmed}
+		type="password"
+		id="confirm_password"
+		name="confirm_password"
+		required
+	/><br />
+	{#if passwordsNotEqual}
+		<p style="color: red;">Passwords do not match!</p>
+	{:else}
+		<br />
+	{/if}
+	<button disabled={!password || passwordsNotEqual} type="submit">Sign Up</button>
 </form>
