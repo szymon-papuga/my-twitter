b028ce4b78970dd6ef1d1bbb7ad6b5c5d7b219d7
Szymon Papuga <papuga.szymon@gmail.com>
2025-01-20 23:17:37 +0100

    dockerize e2e tests

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		// baseURL: 'http://127.0.0.1:3000',
+		baseURL: `http://localhost:${process.env.VITE_PORT}`,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry'
@@ -79,12 +79,11 @@ export default defineConfig({
 		//   name: 'Google Chrome',
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
-	]
+	],
 
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+		command: 'npm run dev',
+		port: process.env.VITE_PORT,
+		reuseExistingServer: !process.env.CI
+	}
 });
--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -10,12 +10,12 @@ const authFile = path.join(
 
 setup('authenticate', async ({ page }) => {
 	// Perform authentication steps.
-	await page.goto('localhost:5173/login');
+	await page.goto('/login');
 	await page.getByLabel('E-mail').fill('test_user@gmail.com');
 	await page.getByLabel('password').fill('password');
 	await page.getByRole('button', { name: 'Login' }).click();
 	// Wait for the final URL to ensure that the cookies are actually set.
-	await page.waitForURL('http://localhost:5173/tweets');
+	await page.waitForURL('/tweets');
 	// End of authentication steps.
 	await page.context().storageState({ path: authFile });
 });
