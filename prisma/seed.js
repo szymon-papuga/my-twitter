f4283c0ba444c6bedc348c9d9a35ae7fc8929bc0
Szymon Papuga <papuga.szymon@gmail.com>
2025-02-06 23:21:43 +0100

    e2e-oriented seeding

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,19 +1,33 @@
 import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
-const tweets = Array.from({ length: 50 }).map(() => ({
-	text: faker.lorem.words({ min: 1, max: 100 }).substring(0, 140),
-	user: {
-		create: {
-			name: faker.person.fullName(),
-			email: faker.internet.email(),
-			password: faker.internet.password()
-		}
+const tweets = [
+	{
+		text: 'This tweet is meant to be located by Playwright test',
+		user: {
+			create: {
+				name: faker.person.fullName(),
+				email: faker.internet.email(),
+				password: faker.internet.password()
+			}
+		},
+		creationTime: new Date()
 	},
-	creationTime: faker.date.recent()
-}));
+	...Array.from({ length: 50 }).map(() => ({
+		text: faker.lorem.words({ min: 1, max: 100 }).substring(0, 140),
+		user: {
+			create: {
+				name: faker.person.fullName(),
+				email: faker.internet.email(),
+				password: faker.internet.password()
+			}
+		},
+		creationTime: faker.date.recent()
+	}))
+];
 
 async function main() {
 	const tweetCount = await prisma.tweet.count();
@@ -24,6 +38,16 @@ async function main() {
 	}
 
 	console.log(`Start seeding...`);
+
+	console.log('Creating test user');
+
+	await prisma.user.create({
+		data: {
+			name: 'test user',
+			email: 'test_user@gmail.com',
+			password: await bcrypt.hash('password', 10)
+		}
+	});
 
 	for (const t of tweets) {
 		const tweet = await prisma.tweet.create({
